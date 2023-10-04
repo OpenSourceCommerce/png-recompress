697cfaf789eaebe3caff5f7bf1c69361e81b25aa
Nhat Do <nhat.do@nfq.com>
2023-10-04 14:26:18 +0700

    fixes SP-591 - fix png dssim high memory usage - update dssim lib - update pngquant lib

--- test/test.js
+++ test/test.js
@@ -4,11 +4,11 @@ const compareSize = require('compare-size');
 const test = require('ava');
 
 test('minify a PNG', async t => {
-	t.timeout(180 * 1000);
-	const src = path.resolve(__dirname, './test.png');
-	const dest = path.resolve(__dirname, './final.png');
-	await pngRecompress(40, 80, src, dest);
-	const result = await compareSize(src, dest);
+    t.timeout(180 * 1000);
+    const src = path.resolve(__dirname, './test.png');
+    const dest = path.resolve(__dirname, './final.png');
+    await pngRecompress(40, 98, src, dest);
+    const result = await compareSize(src, dest);
 
-	t.true(result[dest] < result[src]);
+    t.true(result[dest] < result[src]);
 });
